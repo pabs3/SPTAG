5d5dbd8d7e572f484da7d08bcdc96892e9be4345
Paul Wise <pabs3@bonedaddy.net>
2023-04-01 15:58:05 +0800

    Revert "Allow setting a different Logger at runtime  (#370)"
    This reverts commit ac85ff4029316d9d77f98845969117274ae6f71a.

--- AnnService/src/Core/VectorIndex.cpp
+++ AnnService/src/Core/VectorIndex.cpp
@@ -15,29 +15,22 @@ typedef typename SPTAG::Helper::Concurrent::ConcurrentMap<std::string, SPTAG::Si
 
 using namespace SPTAG;
 
-Helper::LoggerHolder& SPTAG::GetLoggerHolder() {
+std::shared_ptr<Helper::Logger> SPTAG::GetLogger() {
 #ifdef DEBUG
-    auto logLevel = Helper::LogLevel::LL_Debug;
+  auto logLevel = Helper::LogLevel::LL_Debug;
 #else
-    auto logLevel = Helper::LogLevel::LL_Info;
+  auto logLevel = Helper::LogLevel::LL_Info;
 #endif
 #ifdef  _WINDOWS_
-    if (auto exeHandle = GetModuleHandleW(nullptr)) {
-        if (auto SPTAG_GetLoggerLevel = reinterpret_cast<SPTAG::Helper::LogLevel(*)()>(GetProcAddress(exeHandle, "SPTAG_GetLoggerLevel"))) {
-            logLevel = SPTAG_GetLoggerLevel();
-        }
+  if (auto exeHandle = GetModuleHandleW(nullptr)) {
+    if (auto SPTAG_GetLoggerLevel = reinterpret_cast<SPTAG::Helper::LogLevel(*)()>(GetProcAddress(exeHandle, "SPTAG_GetLoggerLevel"))) {
+      logLevel = SPTAG_GetLoggerLevel();
     }
+  }
 #endif //  _WINDOWS_
-    static Helper::LoggerHolder s_pLoggerHolder(std::make_shared<Helper::SimpleLogger>(logLevel));
-    return s_pLoggerHolder;
-}
 
-std::shared_ptr<Helper::Logger> SPTAG::GetLogger() {
-    return GetLoggerHolder().GetLogger();
-}
-
-void SPTAG::SetLogger(std::shared_ptr<Helper::Logger> p_logger) {
-    GetLoggerHolder().SetLogger(p_logger);
+  static std::shared_ptr<Helper::Logger> s_pLogger = std::make_shared<Helper::SimpleLogger>(logLevel);
+  return s_pLogger;
 }
 
 std::mt19937 SPTAG::rg;
